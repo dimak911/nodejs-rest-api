3b0274893807d683db48fbc3c08e8fee46dc745a
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-11-19 13:28:59 +0200

    fix tests

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,47 +1,90 @@
 const app = require("../src/app");
 const mongoose = require("mongoose");
-const { DB_HOST } = require("../src/config");
+const { DB_HOST_TEST } = require("../src/config");
 const request = require("supertest");
+const User = require("../src/models/schemas/user");
 
-const baseURL = "http://localhost:3002";
+const correctUser = {
+  email: "test1@mail.com",
+  password: "123456",
+};
 
-describe("POST /login", () => {
+const wrongEmailUser = {
+  email: "test1@com",
+  password: "123456",
+};
+
+const shortPassUser = {
+  email: "test1@mail.com",
+  password: "12345",
+};
+
+describe("signup", () => {
   beforeAll(async () => {
-    try {
-      await mongoose.connect(DB_HOST);
-      console.log("Database connection successful");
-
-      app.listen(3002, () => {
-        console.log("Server running. Use our API on port: 3002");
-      });
-    } catch (err) {
-      console.error("Failed to start server with error: ", err.message);
-      process.exit(1);
-    }
+    await mongoose.connect(DB_HOST_TEST);
   });
 
-  afterAll(() => {
-    mongoose.connection.close();
-
-    function gracefulshutdown() {
-      console.log("Shutting down");
-      app.close(() => {
-        console.log("HTTP server closed.");
-
-        process.exit(0);
-      });
-    }
-
-    process.on("SIGTERM", gracefulshutdown);
+  afterAll(async () => {
+    await mongoose.disconnect();
   });
 
-  const user = {
-    email: "test4@mail.com",
-    password: "123456",
-  };
+  it("success signup", async () => {
+    const response = await request(app)
+      .post("/api/users/signup")
+      .send(correctUser);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toStrictEqual({
+      user: { email: "test1@mail.com", subscription: "starter" },
+    });
+  });
+
+  it("wrong email signup", async () => {
+    const response = await request(app)
+      .post("/api/users/signup")
+      .send(wrongEmailUser);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      message: '"email" must be a valid email',
+    });
+  });
+
+  it("short password signup", async () => {
+    const response = await request(app)
+      .post("/api/users/signup")
+      .send(shortPassUser);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      message: '"password" length must be at least 6 characters long',
+    });
+  });
+
+  it("only uniq email signup", async () => {
+    const response = await request(app)
+      .post("/api/users/signup")
+      .send(correctUser);
+
+    expect(response.status).toBe(409);
+    expect(response.body).toStrictEqual({ message: "Email in use" });
+  });
+});
+
+describe("login", () => {
+  beforeAll(async () => {
+    await mongoose.connect(DB_HOST_TEST);
+  });
+
+  afterAll(async () => {
+    await User.deleteMany();
+    await mongoose.disconnect();
+  });
 
   it("success login", async () => {
-    const response = await request(baseURL).post("/api/users/login").send(user);
+    const response = await request(app)
+      .post("/api/users/login")
+      .send(correctUser);
 
     expect(response.status).toBe(200);
   });
--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
-const { PORT = 3000, DB_HOST, SECRET } = process.env;
+const { PORT = 3000, DB_HOST, DB_HOST_TEST, SECRET } = process.env;
 
 module.exports = {
   PORT,
   DB_HOST,
   SECRET,
+  DB_HOST_TEST,
 };
